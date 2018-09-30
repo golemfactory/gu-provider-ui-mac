3e8e3f653621756d91357d26e220b3767e6b8180
PRZEMYSLAW REKUCKI <prekucki@przemyslaws-macbook-pro.local>
2018-09-30 08:45:37 +0200

    Initial Commit

---
+++ gu-provider/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  gu-provider
+//
+//  Created by PRZEMYSLAW REKUCKI on 30/09/2018.
+//  Copyright © 2018 Golem Factory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
