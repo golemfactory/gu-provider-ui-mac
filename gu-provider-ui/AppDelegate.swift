a45d33e9aaf5932fa0e12d621c9ace473f9c025f
Filip <filip@golem.network>
2019-10-30 10:12:25 +0100

    Set temporary directory in portable mode

--- gu-provider-ui/AppDelegate.swift
+++ gu-provider-ui/AppDelegate.swift
@@ -388,9 +388,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDelegate, NSTable
                 withIntermediateDirectories: true,
                 attributes: nil
             )
-            try? """
-                {"workDir":"../data", "cacheDir":"../cache", "runtimeDir":"../run"}
-            """.write(to: configDir.appendingPathComponent("dir-paths.json"), atomically: true, encoding: .utf8)
+            try? ("{"
+                + "\"workDir\":\"../data\","
+                + "\"cacheDir\":\"../cache\","
+                + "\"runtimeDir\":\"../run\","
+                + "\"tmpDir\":\"../tmp\""
+                + "}").write(to: configDir.appendingPathComponent("dir-paths.json"), atomically: true, encoding: .utf8)
+            //"""
+            //try? """
+            //    {"workDir":"../data", "cacheDir":"../cache", "runtimeDir":"../run"}
+            //""".write(to: configDir.appendingPathComponent("dir-paths.json"), atomically: true, encoding: .utf8)
             process.arguments = ["-vv", "-c", configDir.path, "server", "run"]
         } else {
             process.arguments = ["-vv", "server", "run", "--user"]
